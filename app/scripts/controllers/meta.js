74fcdeeb4f94522c037deb5d75fd2df1861a0208
lakemove <lakemove@gmail.com>
2014-07-31 15:42:46 +0800

    more or less completed meta, and dynamic sidebar

--- app/scripts/controllers/meta.js
+++ app/scripts/controllers/meta.js
@@ -9,28 +9,51 @@
  */
 angular.module('cmsAppApp').controller('metaController', ['$scope', '$modal', 'metaResource', function ($scope, $modal, metaResource) {
 
+    $scope.filter = {};
+
     metaResource.query({}, function (items) {
         $scope.metas = items;
     });
 
+    $scope.query = function () {
+        var c = {criteria: {}};
+        if ($scope.filter.type) {
+            c.criteria.type = $scope.filter.type;
+        }
+        metaResource.query(c, function (items) {
+            $scope.metas = items;
+        });
+    }
+
     $scope.newMeta = function () {
-        $scope.edit = {};
+        $scope.meta = {};
+    }
+
+    metaResource.query({criteria: {type: 'type'}}, function (items) {
+        $scope.types = items.map(function (i) {return i.value;});
+    });
+
+    $scope.isType = function (t) {
+        $scope.meta.type == t.value;
     }
 
     $scope.save = function () {
-        if ($scope.edit._id) {
-            $scope.edit.$update(function () {
-                console.log('success');
+        var meta = $scope.meta;
+        if (meta._id) {
+            meta.$update().$promise.then(function () {
+                //TODO handle success 
+            }).fail(function (res) {
+                //TODO handle failure
             });
         } else {
-            metaResource.save($scope.edit, function (item) {
+            metaResource.save(meta, function (item) {
                 $scope.metas.unshift(item);
             });
         }
     }
 
     $scope.editMeta = function(meta) {
-        $scope.edit = meta;
+        $scope.meta = meta;
     }
 
     $scope.delMeta = function(meta) {

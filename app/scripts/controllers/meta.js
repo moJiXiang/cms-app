a0a67422b70cacee2156fe4e2b1270f4e1581a6e
lakemove <lakemove@gmail.com>
2014-08-03 13:36:53 +0800

    fix meta update

--- app/scripts/controllers/meta.js
+++ app/scripts/controllers/meta.js
@@ -40,9 +40,10 @@ angular.module('cmsAppApp').controller('metaController', ['$scope', '$modal', 'm
     $scope.save = function () {
         var meta = $scope.meta;
         if (meta._id) {
-            meta.$update().$promise.then(function () {
-                //TODO handle success 
-            }).fail(function (res) {
+            meta.$update().then(function (ab) {
+                $scope.meta = ab;
+            }).catch(function (res) {
+                console.log(res);
                 //TODO handle failure
             });
         } else {

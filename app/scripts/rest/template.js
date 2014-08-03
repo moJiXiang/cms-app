c5129814d0f1455a5ffa23b209ce85bafbd780f5
lakemove <lakemove@gmail.com>
2014-08-03 22:45:55 +0800

    count

--- app/scripts/rest/template.js
+++ app/scripts/rest/template.js
@@ -4,7 +4,7 @@ app.factory('userResource', ['$resource', function ($resource) {
 
     var Model =  $resource('/rest/users/:id', {id:'@_id'}, {
         query : {method: 'GET', isArray: true, transformResponse: unwrap},
-        count : {method: 'GET', params: {cmd:'countNum'}},
+        count : {method: 'GET', params: {countNum:true}},
         get   : {method: 'GET', transformResponse: unwrap},
         update: {method: 'PUT', transformResponse: unwrap}
     });

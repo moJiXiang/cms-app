de93c56d328c6b2a120291e3edc424458fa76c4c
moJiXiang <moji_xiang@163.com>
2014-09-01 13:38:33 +0800

    restaurants sort by cityname

--- app/scripts/controllers/restaurant.js
+++ app/scripts/controllers/restaurant.js
@@ -25,7 +25,7 @@ angular.module('cmsAppApp')
             restaurantResource.query({
                 city_name: $scope.cityname,
                 offset: ($scope.currentPage - 1) * 20,
-                sort: "city_name, -show_flag, -index_flag, ranking"
+                sort: "-city_name, -show_flag, -index_flag, ranking"
             }, function(items) {
                 items.forEach(function(item) {
                     item.imagecount = item.image.length;
@@ -46,7 +46,7 @@ angular.module('cmsAppApp')
             })
             return restaurantResource.query({
                 city_name: val,
-                sort: "city_name, -show_flag, -index_flag, ranking"
+                sort: "-city_name, -show_flag, -index_flag, ranking"
             }, function (items) {
                 items.forEach(function(item) {
                     item.imagecount = item.image.length;
